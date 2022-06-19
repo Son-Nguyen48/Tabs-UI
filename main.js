69f9dfd7146620bc295d74e7a8c6d577ed711238
Son-Nguyen48 <sonnt.B16CN303@stu.ptit.edu.vn>
2022-06-19 20:53:36 +0700

    Hiep performance

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 let tabsNode = document.getElementsByClassName("tab");
 let contentsNode = document.getElementsByClassName("content");
-let lineBorder = document.querySelector(".line-border");
 
 let tabsLength = tabsNode.length;
 
 for (let index1 = 0; index1 < tabsLength; index1++) {
   tabsNode[index1].onclick = function () {
-    tabsNode[index1].classList.add("active");
-    contentsNode[index1].classList.add("active");
-    Object.assign(lineBorder.style, {
-      width: "25%",
-      left: `${index1 * 25}%`
-    });
-    for (let index2 = 0; index2 < tabsLength; index2++) {
-      if (index2 !== index1) {
-        if (tabsNode[index2].classList.contains("active")) {
-          contentsNode[index2].classList.remove("active");
-          tabsNode[index2].classList.remove("active");
-        }
-      }
+    let activeTab = document.querySelector(".active");
+    let activeContent = document.querySelector(".active-content");
+
+    if (activeTab.classList.contains("active")) {
+      activeTab.classList.remove("active");
     }
+    if (activeContent.classList.contains("active-content")) {
+      activeContent.classList.remove("active-content");
+    }
+
+    tabsNode[index1].classList.add("active");
+    contentsNode[index1].classList.add("active-content");
   };
 }
